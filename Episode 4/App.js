71b3cae5833af1bfee7a1dff7f10a7b52b4d4f13
DHARM <opdsbanasya@gmail.com>
2024-08-03 17:48:16 +0530

    Episode 4 initialised

---
+++ Episode 4/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const Header = ()=>{
+    return <header>
+        <div>
+            <img src="" />
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    </header>
+}
+
+const AppLay = () =>{
+    
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLay />);
+

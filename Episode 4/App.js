9ade92fd43937d2b0da73820bdb6c8b62037ce56
DHARM <opdsbanasya@gmail.com>
2024-08-03 18:14:17 +0530

    header created

--- Episode 4/App.js
+++ Episode 4/App.js
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 
 const Header = ()=>{
     return <header>
-        <div>
-            <img src="" />
+        <div id="logo-container">
+            <img id="logo" src="https://imgs.search.brave.com/hkG-MNTaxFwfJGO7_WouUVHTfPzCYqLO1xWUnzur-_w/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtcGxhdGZvcm0u/OTlzdGF0aWMuY29t/Ly9zXy0ycWdSclBa/bldVcFdJaDROZUl1/OTVVQ0U9LzB4MDo5/OTl4OTk5L2ZpdC1p/bi81MDB4NTAwLzk5/ZGVzaWducy1jb250/ZXN0cy1hdHRhY2ht/ZW50cy8xMTgvMTE4/NjEyL2F0dGFjaG1l/bnRfMTE4NjEyOTQz.jpeg" />
         </div>
         <nav>
             <ul>
@@ -19,7 +19,9 @@ const Header = ()=>{
 }
 
 const AppLay = () =>{
-    
+    return <>
+        <Header />
+    </>
 }
 
 

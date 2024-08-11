e1cbf7cfd127e41ad431d79e9795a2f21c6393f6
DHARM <opdsbanasya@gmail.com>
2024-08-11 21:03:58 +0530

    all router pages created and also errorPage added

--- Episode 7/src/App.js
+++ Episode 7/src/App.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-
+import About from "./Components/About";
+import ContactUs from "./Components/ContactUs";
+import ErrorPage from "./Components/ErrorPage";
 
 
 const App = () => {
@@ -16,15 +18,19 @@ const App = () => {
 const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        errorElement: <ErrorPage />
     },
-    {}
-    
-
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/contact",
+        element: <ContactUs />
+    },
 ])
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
-
+root.render(<RouterProvider router={appRouter} />);
---
+++ Episode 7/src/Components/ErrorPage.js
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Ooops!!!</h1>
+            <h2>Someting went wrong!!</h2>
+        </div>
+    );
+}
+
+export default ErrorPage;

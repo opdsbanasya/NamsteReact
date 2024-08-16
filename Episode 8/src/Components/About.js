64fc59d186f5ee0715dbc2f4be6f4350e4fe3eed
DHARM <opdsbanasya@gmail.com>
2024-08-16 17:34:07 +0530

    creating a  functinal component and state vars

--- Episode 8/src/Components/About.js
+++ Episode 8/src/Components/About.js
@@ -1,10 +1,12 @@
 import UserCardClass from "./UserCardClass";
+import UserCardFunction from "./UserCardFunction";
 
 const About = () => {
     return (
         <div>
             <h1>About Us</h1>
-            <UserCardClass />
+            <UserCardClass name={"Dharm Singh"} city={"Jaipur"} />
+            <UserCardFunction name={"Dharm Singh"} city={"Jaipur"} />
         </div>
     );
 }
--- Episode 8/src/Components/UserCardClass.js
+++ Episode 8/src/Components/UserCardClass.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class UserCardClass extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+
+        this.state = {
+            status: "active"
+        }
     }
     render(){
+        const {name, city} = this.props;
+        const {status} = this.state;
         return (
             <div className='user-card'>
-                <h2>Name: Dharm Singh</h2>
-                <h3>City: Jaipur</h3>
+                <h2>Name: {name} class <span className="user-status">{status}</span></h2>
+                <h3>City: {city}</h3>
                 <h3>Twitter: @opdsbanasya</h3>
             </div>
         )

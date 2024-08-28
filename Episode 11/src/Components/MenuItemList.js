5ee6cdee51f82b0ff9b0c3ab4876ea5232083f75
DHARM <opdsbanasya@gmail.com>
2024-08-29 00:57:00 +0530

    update menu list

--- Episode 11/src/Components/MenuItemList.js
+++ Episode 11/src/Components/MenuItemList.js
@@ -9,11 +9,11 @@ const MenuItemList = ({itemList}) => {
                 <div key={item.card.info.id} className="w-full h-fit  bg-zinc-100 flex items-center p-5 rounded-md relative">
                     <div className="w-9/12">
                         <h2 className="text-lg font-semibold">{item.card.info.name}</h2>
-                        <h3 className="text-lg font-semibold">{item.card.info.price/100 || item.card.info.defaultPrice/100}</h3>
+                        <h3 className="text-lg font-semibold"> ₹ {item.card.info.price/100 || item.card.info.defaultPrice/100}</h3>
                         <p className="text-sm">{item.card.info.description}</p>
                     </div>
                     {item.card.info.imageId && (
-                        <img src={MENU_IMG + item.card.info.imageId} className="h-40 w-3/12 object-cover rounded-md " />
+                        <img src={MENU_IMG + item.card.info.imageId} alt={item.card.info.name} className="h-40 w-3/12 object-cover rounded-md " />
                     )}
                     <button className="absolute right-[5.5rem] bottom-4 px-1 border-2 border-black hover:bg-black hover:text-white font-bold rounded-lg">Add</button>
 

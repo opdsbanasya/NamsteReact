27edcf575e6b8c54d5f1edeaed636f9c061d9f8b
DHARM <opdsbanasya@gmail.com>
2024-08-29 01:50:52 +0530

    css update

--- Episode 11/src/Components/MenuItemList.js
+++ Episode 11/src/Components/MenuItemList.js
@@ -4,9 +4,9 @@ const MenuItemList = ({itemList}) => {
     // console.log(itemList);
     
     return (
-        <div className="w-full rounded-md space-y-6">
+        <div className="w-full rounded-md px-5">
             {itemList.map(item => (
-                <div key={item.card.info.id} className="w-full h-fit  flex items-center p-5 rounded-md relative">
+                <div key={item.card.info.id} className="w-full h-fit  flex items-center   py-10 relative border-b-2 border-zinc-400 ">
                     <div className="w-9/12">
                         <h2 className="text-lg font-semibold">{item.card.info.name}</h2>
                         <h3 className="text-lg font-semibold"> ₹ {item.card.info.price/100 || item.card.info.defaultPrice/100}</h3>
@@ -15,7 +15,7 @@ const MenuItemList = ({itemList}) => {
                     {item.card.info.imageId && (
                         <img src={MENU_IMG + item.card.info.imageId} alt={item.card.info.name} className="h-40 w-3/12 object-cover rounded-md " />
                     )}
-                    <button className="absolute right-[5.5rem] bottom-4 px-1 border-2 border-black hover:bg-black hover:text-white font-bold rounded-lg">Add</button>
+                    <button className="absolute right-[4.5rem] bottom-10 px-1 border-2 border-black bg-black text-white font-semibold rounded-lg">Add +</button>
 
                     
                 </div>

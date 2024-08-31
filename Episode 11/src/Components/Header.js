b31e1a8f2431ff8bd85b67178c93a729ed8ddd01
DHARM <opdsbanasya@gmail.com>
2024-08-31 19:08:06 +0530

    UserContext creadted

--- Episode 11/src/Components/Header.js
+++ Episode 11/src/Components/Header.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constant";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
 const Header = () => {
 
     const [btnName, setBtnName] = useState("login")
     const userStatus = useOnlineStatus();
+
+    const {userName} = useContext(UserContext);
+    console.log(userName);
+    
 
     return <header className="w-full h-[12vh] flex items-center justify-between px-10 shadow-md mb-4">
         <div className="w-1/3 h-full">
@@ -19,6 +24,7 @@ const Header = () => {
                 <li className="text-lg hover:underline font-semibold"><Link to="/contact">Contact Us</Link></li>
                 <li className="text-lg hover:underline font-semibold"><Link to="/grocery">Grocery</Link></li>
                 <li className="text-lg hover:underline font-semibold">Cart</li>
+                <li className="text-lg hover:underline font-semibold">{userName}</li>
                 <button className="w-20 px-4 py-2 bg-blue-500 text-white rounded-lg" onClick={() => { 
                     btnName === "login" ? 
                     setBtnName("logout") : 
